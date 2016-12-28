12f14d738b88a413c0d491e4ecc54b6ee107fc83
madhusudhanv <madhusudhan.vundela@imaginea.com>
2016-12-28 16:20:43 +0530

    Fixed minor bug in logger.js file

--- src/logger.js
+++ src/logger.js
@@ -14,15 +14,33 @@ const esTransportOpts = {
 	consistency : false
 };
 
-export default function () {
-	if (logger === null) {
-		console.log('initializing the logger');
-		logger = new winston.Logger({
-			transports: [
-              new(winston.transports.Console)(),
-              new WinElastic(esTransportOpts)
-          ]
-		});
-	}
-	return logger;
+export default function() {
+    if (logger === null) {
+        console.log('initializing the logger');
+        logger = new winston.Logger({
+            transports: [
+                new(winston.transports.Console)()
+            ]
+        });
+				elasticConnectStatus(err => {
+					if(!err) {
+						logger.add(winston.transports.Elasticsearch, esTransportOpts);
+					}
+				});
+    }
+    return logger;
+}
+
+function elasticConnectStatus (callback) {
+	client.ping({
+	    requestTimeout: 3000
+	}, (error) => {
+	    if (error) {
+	        console.trace('elasticsearch is not running!. Not adding elastic transport');
+					callback(error);
+			} else {
+	        console.log('All is well');
+					callback(null);
+	    }
+	});
 }
